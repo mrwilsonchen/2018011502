987106bc8ad730092e2ca91c22617634081a37bd
C1 <c1@gjun.com>
2018-01-15 12:16:20 +0800

    3

--- app/src/main/java/com/wilson/a2018011502/MainActivity.java
+++ app/src/main/java/com/wilson/a2018011502/MainActivity.java
@@ -1,13 +1,90 @@
 package com.wilson.a2018011502;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img;
+    TextView tv;
+    ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
+        tv = (TextView) findViewById(R.id.textView);
+        pb=(ProgressBar) findViewById(R.id.progressBar);
+    }
+    public void click1(View v)
+    {
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                String str_url = "https://5.imimg.com/data5/UH/ND/MY-4431270/red-rose-flower-500x500.jpg";
+                URL url;
+                try {
+                    url = new URL(str_url);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.connect();
+                    InputStream inputStream = conn.getInputStream();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                    byte[] buf = new byte[1024];
+                    final int totalLength = conn.getContentLength();
+                    int sum = 0;
+                    int length;
+                    while ((length = inputStream.read(buf)) != -1)
+                    {
+                        sum += length;
+                        final int tmp = sum;
+                        bos.write(buf, 0, length);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                tv.setText(String.valueOf(tmp) + "/" + totalLength);
+                                pb.setProgress(100*tmp/totalLength);
+                            }
+                        });
+                    }
+                    byte[] results = bos.toByteArray();
+                    final Bitmap bmp = BitmapFactory.decodeByteArray(results, 0, results.length);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            img.setImageBitmap(bmp);
+                        }
+                    });
+
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
     }
 }
